c39480151c206366c6b88220c032ae1b9d56a4d8
abyssrift <161496639+abyssrift@users.noreply.github.com>
2024-10-26 20:33:09 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -2,73 +2,70 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <iomanip>
+#include <bitset>
+#include <string>
+#include<sstream>
+
 using namespace std;
 
-class RegisterSingle {
+
+
+class RegisterSingle
+{
 public:
 	void SetRegisterSingle(string s) {
-		cout << "Setting RegisterSingle to: " << s << endl;
 		value = s;
 	}
 	void SetRegisterSingle(int n, char s) {
-		cout << "Setting RegisterSingle at index " << n << " to: " << s << endl;
 		value[n] = s;
 	}
 	string ReturnValue() {
-		cout << "Returning RegisterSingle value: " << value << endl;
 		return value;
 	}
-	RegisterSingle() {
+	RegisterSingle()
+	{
 		value = "0000";
-		cout << "RegisterSingle initialized with value: " << value << endl;
+		cout << "RegisterSingle successfully initiated" << endl;
 	}
+
 protected:
 	string value;
 };
 
-class InstructionRegister : public RegisterSingle {
+class InstructionRegister :public RegisterSingle {
 private:
 	int Direction = 0;
-	vector<char> ControlUnitOperations{ '1', '2', '3', '4', 'B', 'C' };
-	vector<char> ALUOperations{ '5', '6' };
-public:
-	bool Loaded;
+	vector<char> ControlUnitOperations{ '1','2','3','4','B','C' };
+	vector<char>ALUOperations{ '5','6' };
 
+public:
+	string GetInstruction() {
+		return value;
+	}
 	int GetDirection() {
-		cout << "Getting Direction from InstructionRegister: " << Direction << endl;
 		return Direction;
+	}
+	void SetDirection(int value) {
+		Direction = value;
 	}
 
 	void decode() {
-		cout << "Decoding instruction: " << value[0] << endl;
 		auto it = find(ControlUnitOperations.begin(), ControlUnitOperations.end(), value[0]);
-		if (it != ControlUnitOperations.end()) {
+		if (it != ControlUnitOperations.end())
+		{
 			Direction = 1;
-			cout << "Passing instruction to Control Unit" << endl;
-			PassInstructionToCU();
 		}
-		else {
-			it = find(ALUOperations.begin(), ALUOperations.end(), value[0]);
+		else
+		{
+			auto it = find(ALUOperations.begin(), ALUOperations.end(), value[0]);
 			if (it != ALUOperations.end()) {
 				Direction = 2;
-				cout << "Passing instruction to ALU" << endl;
-				PassInstructionsToALU();
 			}
 		}
-		cout << "Resetting Direction and Loaded status in InstructionRegister" << endl;
-		Direction = 0;
-		Loaded = false;
-		value = "0000";
 	}
 	InstructionRegister() {
-		Loaded = false;
-		cout << "InstructionRegister initialized" << endl;
-	}
-	void PassInstructionToCU() {
-		cout << "Instruction passed to Control Unit" << endl;
-	}
-	void PassInstructionsToALU() {
-		cout << "Instruction passed to ALU" << endl;
+		cout << "InstruReg successfully initiated" << endl;
 	}
 };
 
@@ -78,28 +75,25 @@ private:
 public:
 	Register() {
 		RegisterVector.resize(16);
-		cout << "Register initialized with 16 RegisterSingle slots" << endl;
+		cout << "Register_16 successfully initiated" << endl;
 	}
 	void SetRegisterSingle(int index, string s) {
-		cout << "Setting RegisterSingle at index " << index << " with value: " << s << endl;
 		RegisterVector[index].SetRegisterSingle(s);
 	}
 	RegisterSingle GetRegister(int index) {
-		cout << "Fetching RegisterSingle at index: " << index << endl;
 		return RegisterVector[index];
 	}
 	string GetRegisterValue(int index) {
-		cout << "Getting value from RegisterSingle at index: " << index << endl;
 		return RegisterVector[index].ReturnValue();
 	}
 };
 
+//main memory where the memory slots contain address and value
 class Memory {
 private:
 	vector<string> memoryslots;
 public:
 	void SetMemoryValue(int address, string value) {
-		cout << "Setting memory at address " << address << " to value: " << value << endl;
 		memoryslots[address] = value;
 	}
 	string GetMemoryValue(int address) {
@@ -107,31 +101,33 @@ public:
 		return memoryslots[address];
 	}
 	void ClearSlot(int address) {
-		cout << "Clearing memory slot at address " << address << endl;
 		memoryslots[address] = "00";
 	}
 	void ResizeMemory(int memorysize) {
-		cout << "Resizing memory to size: " << memorysize << endl;
 		memoryslots.resize(memorysize);
 	}
 	void Clear() {
-		cout << "Clearing all memory slots" << endl;
-		for (int i = 0; i < memoryslots.size(); i++) {
+		for (int i = 0; i < memoryslots.size(); i++)
+		{
 			memoryslots[i] = "00";
 		}
 	}
 	Memory() {
 		memoryslots.resize(256, "00");
-		cout << "Memory initialized with 256 slots set to '00'" << endl;
+		cout << "Memory successfully initiated" << endl;
 	}
 };
 
+
 class ProgramCounter {
-	Memory mainMemory;
+	Memory& mainMemory;
 	string instruc;
 	int counter = 0;
-	InstructionRegister instructionReg;
+	InstructionRegister& instructionReg;
 public:
+	void GetMemory() {
+
+	}
 	bool isEndOfProgram() {
 		cout << "Checking if end of program with instruction: " << instruc << endl;
 		return instruc == "C000";
@@ -139,7 +135,6 @@ public:
 	void PassInstruction() {
 		cout << "Passing instruction to InstructionRegister: " << instruc << endl;
 		instructionReg.SetRegisterSingle(instruc);
-		instructionReg.Loaded = true;
 	}
 	void runInstructions() {
 		instruc = (mainMemory.GetMemoryValue(counter) + mainMemory.GetMemoryValue(counter + 1));
@@ -151,166 +146,218 @@ public:
 		counter += 2;
 		PassInstruction();
 	}
-	ProgramCounter(Memory& mainMemory, InstructionRegister& instructionRegister)
-		: mainMemory(mainMemory), instructionReg(instructionRegister) {
+	ProgramCounter(Memory& mainMemory2, InstructionRegister& instructionRegister)
+		: mainMemory(mainMemory2), instructionReg(instructionRegister) {
 		cout << "ProgramCounter initialized" << endl;
 	}
 };
 
+
+
 class ALU {
-	Memory* mainmemory;
+	Memory& mainmemory;
 	Register Register_16;
-	InstructionRegister instruRegister;
-	string instruction;
+	InstructionRegister& instruRegister;
+	string& instruction;
 
 public:
-	ALU(Memory& memory, InstructionRegister& instruReg, Register& register_16)
-		: mainmemory(&memory), instruRegister(instruReg), Register_16(register_16) {
-		cout << "ALU initialized" << endl;
+	ALU(Memory& memory, InstructionRegister& instruReg, Register& register_16, string& instruction2) : mainmemory(memory), instruRegister(instruReg), Register_16(register_16), instruction(instruction2) {
+		cout << "ALU successfully initiated" << endl;
 	}
 	void GetMemory(Memory& memory) {
-		mainmemory = &memory;
-		cout << "Memory set in ALU" << endl;
+		mainmemory = memory;
+	}
+	void SetInstruction(string instru) {
+		instruction = instru;
 	}
 	void Decode() {
-		cout << "Decoding ALU instruction: " << instruction << endl;
 		if (instruction[0] == '5') {
-			cout << "Performing ALU operation for code '5'" << endl;
+
 		}
 		else if (instruction[0] == '6') {
-			cout << "Performing ALU operation for code '6'" << endl;
+
 		}
 	}
 };
 
+//control unit where main instructions of the machine are carried out
 class ControlUnit {
 private:
-	Memory* mainmemory;
+	Memory& mainmemory;
 	Register Register_16;
-	InstructionRegister instruRegister;
-	string instruction;
+	InstructionRegister& instruRegister;
+	string& instruction;
 public:
-	ControlUnit(Memory& memory, InstructionRegister& InstruReg, Register& register_16)
-		: instruRegister(InstruReg), Register_16(register_16), mainmemory(&memory) {
-		cout << "Control Unit initialized" << endl;
+	ControlUnit(Memory& memory, InstructionRegister& InstruReg, Register& register_16, string& instruction2) : instruRegister(InstruReg), Register_16(register_16), mainmemory(memory), instruction(instruction2) {
+		cout << "CU successfully initiated" << endl;
 	}
 
 	void GetMemory(Memory& memory) {
-		mainmemory = &memory;
-		cout << "Memory set in Control Unit" << endl;
+		mainmemory = memory;
 	}
-
+	void CoutInstruction() {
+		cout << instruction << endl;
+	}
 	void Decode() {
-		cout << "Decoding Control Unit instruction: " << instruction << endl;
 		if (instruction[0] == '1') {
-			cout << "Performing operation for code '1'" << endl;
+
 		}
 		else if (instruction[0] == '2') {
-			cout << "Performing operation for code '2'" << endl;
+
 		}
 		else if (instruction[0] == '3') {
-			cout << "Performing operation for code '3'" << endl;
+
 		}
 		else if (instruction[0] == '4') {
-			cout << "Performing operation for code '4'" << endl;
+
 		}
 		else if (instruction[0] == 'A') {
-			cout << "Performing operation for code 'A'" << endl;
+
 		}
 		else if (instruction[0] == 'B') {
-			cout << "Performing operation for code 'B'" << endl;
+
 		}
 	}
 
+	//load Register from memory
 	void Load(int address, int RegisterSinglenumber) {
-		cout << "Loading value from memory address " << address << " to RegisterSingle " << RegisterSinglenumber << endl;
-		Register_16.SetRegisterSingle(RegisterSinglenumber, mainmemory->GetMemoryValue(address));
+		Register_16.SetRegisterSingle(RegisterSinglenumber, mainmemory.GetMemoryValue(address));
 	}
 	void Load(string value, int RegisterSinglenumber) {
-		cout << "Loading value " << value << " to RegisterSingle " << RegisterSinglenumber << endl;
 		Register_16.SetRegisterSingle(RegisterSinglenumber, value);
 	}
 
 	void Store(int address, int RegisterSinglenumber) {
-		cout << "Storing RegisterSingle " << RegisterSinglenumber << " value to memory address " << address << endl;
-		mainmemory->SetMemoryValue(address, Register_16.GetRegisterValue(RegisterSinglenumber));
+		mainmemory.SetMemoryValue(address, Register_16.GetRegisterValue(RegisterSinglenumber));
 	}
 	void Move(int RegisterSinglenumber1, int RegisterSinglenumber2) {
-		cout << "Moving value from RegisterSingle " << RegisterSinglenumber1 << " to RegisterSingle " << RegisterSinglenumber2 << endl;
 		Register_16.SetRegisterSingle(RegisterSinglenumber2, Register_16.GetRegisterValue(RegisterSinglenumber1));
 	}
 };
 
+
 class CPU {
 private:
+	Memory& mainmemory;
 	ProgramCounter programCounter;
-	Memory mainmemory;
 	InstructionRegister instruReg;
 	Register register_16;
 	ControlUnit cu;
 	ALU alu;
+	string instruction = "0000";
 	int Direction = 0;
 public:
 
-	CPU(Memory& mainmemory)
-		: cu(mainmemory, instruReg, register_16), alu(mainmemory, instruReg, register_16), programCounter(mainmemory, instruReg) {
-		cout << "CPU initialized" << endl;
+	CPU(Memory& mainmemory2)
+		: mainmemory(mainmemory2), cu(mainmemory, instruReg, register_16, instruction),
+		alu(mainmemory, instruReg, register_16, instruction),
+		programCounter(mainmemory, instruReg), instruReg() {
+		cout << "CPU successfully initiated" << endl;
 	}
 
+	ControlUnit& GetControlUnit() {
+		return cu;
+	}
+
+	ALU& GetALU() {
+		return alu;
+	}
+
+	InstructionRegister& GetInstructionReg() {
+		return instruReg;
+	}
+
+	Memory& GetMemory() {
+		return mainmemory;
+	}
+
+
 	void CheckEnd() {
-		cout << "Checking if instruction register is loaded" << endl;
-		if (instruReg.Loaded) {
-			cout << "Instruction register loaded; decoding instruction" << endl;
-			instruReg.decode();
-			Direction = instruReg.GetDirection();
-		}
-		else {
-			cout << "Instruction register not loaded" << endl;
+		if (instruReg.ReturnValue() == "C000") {
+			cout << "program exited by user through instructions" << endl;
+			exit(0);
 		}
 	}
+
+
+	void cycle() {
+		programCounter.runInstructions();
+
+		cout << "Attempted to Fetch From memory" << endl;
+		CheckEnd();
+		instruReg.decode();
+		instruction = instruReg.GetInstruction();
+		Direction = instruReg.GetDirection();
+
+		if (Direction == 1) {
+			cu.Decode();
+			cu.CoutInstruction();
+		}
+		else if (Direction == 2) {
+			alu.Decode();
+		}
+		instruReg.SetDirection(0);
+	}
+
+	void Start() {
+		cycle();
+	}
 };
+
+
 
 class Machine {
 private:
 	Memory mainmemory;
-	CPU maincpu;
+	CPU cpu;
 public:
-	Machine() : maincpu(mainmemory) {
-		cout << "Machine initialized" << endl;
+	Memory& Getmemory() {
+		return mainmemory;
+	}
+	CPU& GetCpu() {
+		return cpu;
+	}
+	Machine() : cpu(mainmemory) {
+		cout << "Machine Successfully Initiated" << endl;
 	}
 	void SetMainMemoryValue(int address, string value) {
-		cout << "Setting machine main memory value at address " << address << " to " << value << endl;
 		mainmemory.SetMemoryValue(address, value);
+	}
+
+	void Start() {
+		cout << "Attempted CPU Start" << endl;
+		cpu.Start();
 	}
 };
 
+
+
 int main() {
 	cout << "Give me file name" << endl;
-	string filename;
-	cin >> filename;
+	string filename = "textcase.txt";
 	fstream file(filename, ios::in);
 	if (!file) {
-		cout << "Invalid file, please try again" << endl;
+		cout << "invalid file try again" << endl;
 		main();
 	}
 	string word;
 
 	Machine machine;
 
-	cout << "Loading instructions from file: " << filename << endl;
 	vector<string> instructions;
 	while (file >> word) {
 		instructions.push_back(word.substr(0, 2));
 		instructions.push_back(word.substr(2, 4));
-		cout << "Instruction read from file: " << word << endl;
 	}
-	for (int i = 0; i < instructions.size(); i++) {
-		cout << "Setting memory at address " << i << " to value " << instructions[i] << endl;
+	for (int i = 0; i < (instructions.size()); i += 2) {
+		cout << "Setting memory at address " << i << " " << i + 1 << " to value " << instructions[i] << " " << instructions[i + 1] << endl;
 		machine.SetMainMemoryValue(i, instructions[i]);
 		machine.SetMainMemoryValue(i + 1, instructions[i + 1]);
 	}
+	machine.Start();
+	//regex
 
-	cout << "Instructions vector size: " << instructions.size() << endl;
 
-	return 0;
+
+
 }
